e60e6006dbdafc2417a2469359ce55c39bbf39e9
PierrePineau <pierrepineau.pro@gmail.com>
2024-11-25 14:29:22 +0100

    Init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,55 @@
-import type { NextConfig } from "next";
+import { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } from "next/constants";
+import { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+if (isDevelopment) {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+}
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  crossOrigin: "anonymous",
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        locale: false,
+      },
+    ]
+  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         {
+  //           key: "Access-Control-Allow-Origin",
+  //           value: "*", // You can specify the domains you want to allow here
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Methods",
+  //           value: "GET,POST,PUT,DELETE,OPTIONS",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value: "X-Requested-With, Content-Type, Authorization",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
-export default nextConfig;
+const config = (phase: string): NextConfig => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    return withPWA({
+      dest: "public",
+    })(nextConfig);
+  }
+  return nextConfig;
+};
+
+export default config;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,48 @@
 import type { Config } from "tailwindcss";
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+const config: Config = {
+	content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+          DEFAULT: "var(--color1-500)",
+					100: "var(--color1-100)",
+					200: "var(--color1-200)",
+					300: "var(--color1-300)",
+					400: "var(--color1-400)",
+					500: "var(--color1-500)",
+					main: "var(--color1-500)",
+					600: "var(--color1-600)",
+					700: "var(--color1-700)",
+					800: "var(--color1-800)",
+					900: "var(--color1-900)",
+				},
+				secondary: {
+          DEFAULT: "var(--color2-500)",
+					100: "var(--color2-100)",
+					200: "var(--color2-200)",
+					300: "var(--color2-300)",
+					400: "var(--color2-400)",
+					500: "var(--color2-500)",
+					main: "var(--color2-500)",
+					600: "var(--color2-600)",
+					700: "var(--color2-700)",
+					800: "var(--color2-800)",
+					900: "var(--color2-900)",
+				},
+				dark: "var(--color-dark)",
+				success: "var(--color-success)",
+				warning: "var(--color-warning)",
+				danger: "var(--color-danger)",
+				info: "var(--color-info)",
+			},
+			zIndex: {
+				"-10": "-10",
+				"-20": "-20",
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
