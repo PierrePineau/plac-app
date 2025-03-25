6ec3511ca44a1969928a8e0dfae83e76a5fa5ed5
Leo Teixeira <leo.teixeira@diggers-consulting.com>
2025-03-25 08:35:16 +0100

    login error state and yard error state

--- src/app/(app)/chantiers/components/Table.tsx
+++ src/app/(app)/chantiers/components/Table.tsx
@@ -4,20 +4,18 @@ import { Search } from "lucide-react";
 import { useProjectStore } from "@/store/user/projectStore";
 import Tabs from "@/components/Tabs";
 import { useEffect, useState } from "react";
-import Yard from "@/components/Yard";
-import Spinner from "@/components/Spinner";
+import Yard from "@components/Yard";
+import Spinner from "@components/Spinner";
+import { Alert } from "@heroui/alert";
 
 export default function Table() {
   const [isLoading, setIsLoading] = useState(true);
-  const { data: projects, fetchData } = useProjectStore();
+  const { data: projects, fetchData, error } = useProjectStore();
   const [filteredProjects, setFilteredProjects] = useState(projects);
-
-  const [search, setSearch] = useState<string>("");
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetchData({
-      limit: 100,
-    }).then(() => setIsLoading(false));
+    fetchData({ limit: 100 }).then(() => setIsLoading(false));
   }, []);
 
   useEffect(() => {
@@ -31,38 +29,58 @@ export default function Table() {
     }
   }, [search, projects]);
 
-
-  const renderTabs = (projects?: Project[]) => {
-      return (
-          <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:gap-8">
-              {isLoading ? (
-                  <Spinner message="" />
-              ) : (
-                  projects?.map((project) => (
-                      <Yard key={project.id} project={project} />
-                  ))
-              )}
-          </div>
-      );
-  }
+  const renderTabs = (projects) => {
+    return (
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:gap-8">
+        {isLoading ? (
+          <Spinner message="" />
+        ) : (
+          projects?.map((project) => (
+            <Yard key={project.id} project={project} />
+          ))
+        )}
+      </div>
+    );
+  };
 
   const tabs = [
-    { 
+    {
       label: "Chantiers en cours",
-      content: renderTabs(filteredProjects.filter((project) => project.status?.code === "A_FAIRE" || project.status?.code === "EN_COURS")) 
+      content: renderTabs(
+        filteredProjects.filter(
+          (project) =>
+            project.status?.code === "A_FAIRE" ||
+            project.status?.code === "EN_COURS"
+        )
+      )
     },
     {
       label: "Chantiers à venir",
-      content: renderTabs(filteredProjects.filter((project) => (project.status?.code === "A_FAIRE" || project.status?.code === "EN_COURS") && project.startAt && project.startAt.getTime() > Date.now()))
+      content: renderTabs(
+        filteredProjects.filter(
+          (project) =>
+            (project.status?.code === "A_FAIRE" ||
+              project.status?.code === "EN_COURS") &&
+            project.startAt &&
+            project.startAt.getTime() > Date.now()
+        )
+      )
     },
     {
       label: "Chantiers archivés",
-      content: renderTabs(filteredProjects.filter((project) => project.status?.code === "TERMINE"))
-    } 
+      content: renderTabs(
+        filteredProjects.filter((project) => project.status?.code === "TERMINE")
+      )
+    }
   ];
 
   return (
     <>
+      {error && (
+        <Alert title="Erreur" type="error">
+          {error}
+        </Alert>
+      )}
       <Field
         label="Rechercher un chantier"
         type="search"
