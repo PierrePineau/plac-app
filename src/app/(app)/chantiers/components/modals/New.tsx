361815648c574f0cb735ced73186c91794d31057
Leo Teixeira <leo.teixeira@diggers-consulting.com>
2025-03-25 08:57:33 +0100

    error

--- src/app/(app)/chantiers/components/modals/New.tsx
+++ src/app/(app)/chantiers/components/modals/New.tsx
@@ -5,20 +5,20 @@ import Modal from "@/components/Modal";
 import { useProjectStore } from "@/store/user/projectStore";
 import { Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { Alert } from "@heroui/alert";
 
 export default function New() {
   const router = useRouter();
-  const { create } = useProjectStore();
+  const { create, error } = useProjectStore();
 
   const handleSubmit = async (formData: FormData) => {
     const data = Object.fromEntries(formData.entries());
     const project = await create({
       name: data.name as string,
-      description: data.description as string,  
+      description: data.description as string,
     });
-
     if (project) {
-        router.push(`/chantiers/${project.id}`);
+      router.push(`/chantiers/${project.id}`);
     }
   };
 
@@ -27,19 +27,12 @@ export default function New() {
       title="Ajouter un chantier"
       subtitle="Renseignez les informations sur votre chantier (modifiables par la suite)"
       icon={<Plus />}
-      text={"Ajouter un chantier"}
+      text="Ajouter un chantier"
       onSubmit={handleSubmit}
       store={useProjectStore}>
-      <Field
-        label="Nom du chantier"
-        name="name"
-        isRequired
-      />
-      <Field
-        label="Description"
-        type="textarea"
-        name="description"
-      />
+      <Field label="Nom du chantier" name="name" isRequired />
+      <Field label="Description" type="textarea" name="description" />
+      {error && <Alert color="danger">{error}</Alert>}
     </Modal>
   );
 }
